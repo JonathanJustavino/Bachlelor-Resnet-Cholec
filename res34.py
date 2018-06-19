dcea0c3434d159c551a6dba0e47b67c37011784d
john <s3190208@mail.zih.tu-dresden.de>
2018-06-19 17:06:32 +0200

    Optimize last resnet block

--- res34.py
+++ res34.py
@@ -16,26 +16,21 @@ import datetime
 
 from cholec80 import Cholec80
 
-plt.ion()
+# plt.ion()
 
 IMG_EXTENSIONS = ['.png']
 path = '/media/data/ToolClassification/cholec80/frames'
-folder = '1'
 annotations_path = '/media/data/ToolClassification/cholec80/phase_annotations'
 result_path = '/media/data/ToolClassification/results/resnet34'
 
 
 data_transforms = {
     'train': transforms.Compose([
-        # transforms.RandomResizedCrop(224),
-        # transforms.RandomHorizontalFlip(),
         transforms.Resize((480,270)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
-    'validate': transforms.Compose([
-        # transforms.Resize(256),
-        # transforms.CenterCrop(224),
+    'validate': transforms.Compose([       
         transforms.Resize((480,270)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -51,8 +46,8 @@ def write_epoch_predictions(path, preds, labels):
         results.write(str(labels))
 
 
-training_folder = ['1', '2', '3']
-validation_folder = ['4']
+training_folder = ['4', '2', '3']
+validation_folder = ['1']
 
 training_phase = '3'
 dataset_folders = training_folder + validation_folder
@@ -61,7 +56,7 @@ dataset = {x: Cholec80((os.path.join(path, x)), annotations_path, IMG_EXTENSIONS
 for sub in validation_folder:
     dataset[sub] = Cholec80((os.path.join(path, sub)), annotations_path, IMG_EXTENSIONS, data_transforms['validate'])
 
-loader_batch_size = 64
+loader_batch_size = 72
 
 dataloaders = {x: torch.utils.data.DataLoader(dataset[x], batch_size=loader_batch_size, shuffle=True, num_workers=5) for x in dataset_folders }
 data_sizes = {x: len(dataset[x]) for x in dataset_folders}
@@ -90,11 +85,12 @@ def progress_out(current, total):
     sys.stdout.write("\r[{}/{}]{:.2f}%".format(current, total, (fraction * 100)))
 
 
-def train(model, criterion, optimizer, scheduler, batch_size, learning_rate, epochs=10):
+def train(model, criterion, optimizer, scheduler, batch_size, learning_rate, validation_set, epochs=10):
     since = time.time()
     date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
     predictions = date + "predictions.txt"
     predictions_path = os.path.join(result_path, predictions)
+    print('Validation Set: ', validation_set)
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
@@ -110,11 +106,12 @@ def train(model, criterion, optimizer, scheduler, batch_size, learning_rate, epo
             for set in dataset_folders:
                 d_size = data_sizes[set]
                 print('Set: ', set)
-                if set != '4':
+                if set != validation_set:
                 	# maybe remove the scheduler (only necessary for last top percentages)
                     # scheduler.step()
                     model.train()
                 else:
+                    print('Validation Set', validation_set)
                     model.eval()
                     with open(predictions_path, 'a') as file:
                         file.write("\nEpoch: {}\n".format(epoch))
@@ -134,19 +131,19 @@ def train(model, criterion, optimizer, scheduler, batch_size, learning_rate, epo
 
                     optimizer.zero_grad()
 
-                    with torch.set_grad_enabled(set != '4'):
+                    with torch.set_grad_enabled(set != validation_set):
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
 
-                        if set != '4':
+                        if set != validation_set:
                             loss.backward()
                             optimizer.step()
 
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
 
-                    if set == '4' and epoch % 5 == 0:
+                    if set == validation_set and epoch % 5 == 0:
                         write_epoch_predictions(predictions_path, preds.cpu().numpy(), labels.data.cpu().numpy())
 
                 epoch_loss = running_loss / data_sizes[set]
@@ -154,7 +151,7 @@ def train(model, criterion, optimizer, scheduler, batch_size, learning_rate, epo
 
                 print("{} Loss: {:.4f} Acc: {:.4f}".format(set, epoch_loss, epoch_acc))
 
-                if set == '4' and epoch_acc > best_acc:
+                if set == validation_set and epoch_acc > best_acc:
                     epoch_of_best_acc = epoch
                     best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
@@ -183,14 +180,17 @@ num_ftrs = num_ftrs * 27 # in order to reach the 13.824
 model_conv.fc = nn.Linear(num_ftrs, 7)
 model_conv = model_conv.to(device)
 
+print(model_conv.layer4)
+
 criterion = nn.CrossEntropyLoss()
 learning_rate = 0.001
+validation_set = '1'
 
 # optim Adam
 
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=learning_rate, momentum=0.9)
+optimizer_conv = optim.SGD(model_conv.layer4.parameters(), lr=learning_rate, momentum=0.9)
 
 # maybe later on relevant
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
-model_conv = train(model_conv, criterion, optimizer_conv, exp_lr_scheduler, loader_batch_size, learning_rate, epochs=15)
+model_conv = train(model_conv, criterion, optimizer_conv, exp_lr_scheduler, loader_batch_size, learning_rate, validation_set, epochs=15)
